1af41577f382ad7ee2da4ee0442c010d44daf412
tesla59 <nishant@heim.id>
2022-12-06 23:46:55 +0530

    add homepage and separate every page into separate files

---
+++ lib/homepage.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const HomePage());
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Copain"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text("Something great is waiting to be unleashed"),
+              ElevatedButton(
+                onPressed: null,
+                child: Text("Get Started"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
