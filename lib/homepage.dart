5183a2fec765c17353ffd54e9ddcf63b3cd821c1
tesla59 <nishant@heim.id>
2022-12-07 01:48:56 +0530

    new fonts and color

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'chats.dart' as chats;
 
-void main() => runApp(const HomePage());
-
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
@@ -12,14 +10,32 @@ class HomePage extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Copain"),
+          title: const Text(
+            "Copain",
+            style: TextStyle(fontFamily: 'Roboto Slab'),
+          ),
+          // 0xFF61BBFE
+          // 0xFF4E00FE
+          backgroundColor: const Color(0xFF61BBFE),
         ),
+        backgroundColor: const Color(0xFF242132),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text("Something great is waiting to be unleashed"),
+              const Text(
+                "Something great is waiting to be unleashed",
+                style:
+                    TextStyle(fontFamily: 'Roboto Slab', color: Colors.white),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
               ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.resolveWith(
+                      (states) => const Color(0xFF61BBFE)),
+                ),
                 onPressed: () {
                   Navigator.push(
                       context,
@@ -27,7 +43,10 @@ class HomePage extends StatelessWidget {
                         builder: (context) => const chats.ChatHome(),
                       ));
                 },
-                child: const Text("Get Started"),
+                child: const Text(
+                  "Get Started",
+                  style: TextStyle(fontFamily: 'Roboto Slab'),
+                ),
               ),
             ],
           ),
