aa1378365c4c42d2b975421e0859352a30c3e881
tesla59 <nishant@heim.id>
2022-12-07 02:50:17 +0530

    utils: import http package

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,9 +1,9 @@
 import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 import 'package:flutter/material.dart';
 
 void sendREQ(TextEditingController ctlr, queue) {
-  var http;
   http.post(
     Uri.parse('http://141.148.198.149:7777/newMessage'),
     headers: <String, String>{
